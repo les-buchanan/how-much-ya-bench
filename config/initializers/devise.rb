f135f2604026a4b5c8ab3c5ddb6ab01de39841e3
Les Buchanan <les.buchanan@gmail.com>
2016-01-31 17:18:27 -0700

    Removed callback.

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -260,6 +260,5 @@ Devise.setup do |config|
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
 
-  config.omniauth :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'],
-    calback_url: "http://howmuchyabench.herokuapp.com/auth/facebook/callback"
+  config.omniauth :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
 end
