9bbc942c286df2030f9e635c25054c7f1fdc4286
Les Buchanan <les.buchanan@gmail.com>
2016-02-01 19:45:37 -0700

    Added image to user object.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160123201604) do
+ActiveRecord::Schema.define(version: 20160201043521) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 20160123201604) do
     t.string   "url"
     t.string   "uid"
     t.string   "provider"
+    t.string   "image"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
