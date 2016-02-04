aa072e86e3409e6f0304eceff0e956ee8fd6fc61
Les Buchanan <les.buchanan@gmail.com>
2016-02-04 11:44:45 -0700

    Whoops, forgot a couple things.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  devise :omniauthable, :omniauth_providers => [:facebook, :twitter]
+  devise :omniauthable, :omniauth_providers => [:facebook, :instagram]
 
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
