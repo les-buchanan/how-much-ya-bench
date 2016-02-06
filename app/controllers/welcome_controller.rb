dfaedd6d5e9ee72e5fced963d4e26ffd204f9372
Les Buchanan <les.buchanan@gmail.com>
2016-02-06 16:18:09 -0700

    Added profile pic to welcome page.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,15 +4,15 @@ class WelcomeController < ApplicationController
 
     benchers = User.all_except(current_user).first(10)
     benchers.each do |b|
-      @top_benches << {name: b.name, bench: b.bench, location: b.location}
+      @top_benches << {image: b.image, name: b.name, bench: b.bench, location: b.location}
     end
 
     # fill in the rest
     (10 - @top_benches.count).times do
       rand_bench = Random.rand(210..410).round(-2) + ((Random.rand(1..2) == 1) ? 5 : 0)
-      @top_benches << {name: FFaker::Name.name, bench: rand_bench, location: "#{FFaker::AddressUS.city}, #{FFaker::AddressUS.state}"}
+      @top_benches << {image: nil, name: FFaker::Name.name, bench: rand_bench, location: "#{FFaker::AddressUS.city}, #{FFaker::AddressUS.state}"}
     end
 
-    @top_benches << {name: current_user.name, bench: current_user.bench, location: current_user.location }
+    @top_benches << {image: current_user.image, name: current_user.name, bench: current_user.bench, location: current_user.location }
   end
 end
