fc35ab45d721397c9de029c2210d823a4c6128dc
Les Buchanan <les.buchanan@gmail.com>
2016-02-18 13:07:26 -0700

    Sort benchers by bench_current descending and make limit 50 instead of 10.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,18 +2,9 @@ class WelcomeController < ApplicationController
   def index
     @top_benches = []
 
-    # benchers = User.all_except(current_user).order('bench').first(10)
-    benchers = User.all_except(current_user).first(10)
+    benchers = User.order_by_bench.first(50)
     benchers.each do |b|
       @top_benches << {image: b.profile_image, name: b.name, bench: b.bench, location: b.location}
     end
-
-    # fill in the rest
-    (10 - @top_benches.count).times do
-      rand_bench = Random.rand(210..410).round(-2) + ((Random.rand(1..2) == 1) ? 5 : 0)
-      @top_benches << {image: nil, name: FFaker::Name.name, bench: rand_bench, location: "#{FFaker::AddressUS.city}, #{FFaker::AddressUS.state}"}
-    end
-
-    @top_benches << {image: current_user.profile_image, name: current_user.name, bench: current_user.bench, location: current_user.location }
   end
 end
