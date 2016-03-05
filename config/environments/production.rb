d3415e59083d580a9dbf5fca36d85316cff719d1
jsnbuchanan <jsnbuchanan@gmail.com>
2016-03-05 10:19:31 -0700

    Adding new s3 path to match the new bucket host_name convention.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,8 +80,9 @@ Rails.application.configure do
   # AWS S3 configuration
   config.paperclip_defaults = {
       storage: :s3,
+      s3_host_name: 'howmuchyabench.s3.amazonaws.com',
+      path: '/:class/:attachment/:id_partition/:style/:filename',
       s3_credentials: {
-          url: ':s3_domain_url',
           bucket: ENV['S3_BUCKET_NAME'],
           access_key_id: ENV['AWS_ACCESS_KEY_ID'],
           secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
