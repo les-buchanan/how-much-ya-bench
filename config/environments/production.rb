25c8d64fa9890e0f6f2b526d013548c0190b5db7
jsnbuchanan <jsnbuchanan@gmail.com>
2016-03-05 10:49:51 -0700

    updating the host_name to s3-website-us-west-2.amazonaws.com... again.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,7 +80,7 @@ Rails.application.configure do
   # AWS S3 configuration
   config.paperclip_defaults = {
       storage: :s3,
-      s3_host_name: 's3.amazonaws.com',
+      s3_host_name: 's3-website-us-west-2.amazonaws.com',
       path: '/:class/:attachment/:id_partition/:style/:filename',
       s3_credentials: {
           bucket: ENV['S3_BUCKET_NAME'],
