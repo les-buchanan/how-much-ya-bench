87d0e7523793fe46c9c73eb4f3ce79f31d187a4f
jsnbuchanan <jsnbuchanan@gmail.com>
2016-03-14 21:58:51 -0600

    #21 Removing blank profiles from leaderboard. Adding current user back to leader board even if their profile is blank.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,9 +2,11 @@ class WelcomeController < ApplicationController
   def index
     @top_benches = []
 
-    benchers = User.order_by_bench.first(50)
+    benchers = User.order_by_bench_except(current_user).first(50)
     benchers.each do |b|
       @top_benches << {image: b.profile_image, name: b.name, bench: b.bench, location: b.location}
     end
+
+    @top_benches << {image: current_user.profile_image, name: current_user.name, bench: current_user.bench, location: current_user.location }
   end
 end
